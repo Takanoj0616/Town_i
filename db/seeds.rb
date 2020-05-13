bfcbaff70ed2927afd2e0fcca0d3a6513ca9fe21
JunichiTakano <owner@Takano.local>
2020-05-13 19:15:32 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Sample.create(place: '横浜')
+Sample.create(place: '神奈川')
